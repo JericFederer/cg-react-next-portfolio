545c117bd63bf6ec5ad2edcb7b15fce8e45221eb
JericFederer <pianocoderodyssey@gmail.com>
2024-06-20 17:00:49 +0900

    quote generator added but still testing

--- app/components/GenerateQuote.tsx
+++ app/components/GenerateQuote.tsx
@@ -23,14 +23,7 @@ const GenerateQuote = () => {
         setQuote(quoteObject);
       };
     } catch (error) {
-        const errorQuoteObject = {
-          "text": "The chief task in life is simply this: to identify and separate matters so that I can say clearly to myself which are externals not under my control, and which have to do with the choices I actually control. Where then do I look for good and evil? Not to uncontrollable externals, but within myself to the choices that are my own . . .”
-          "author": "—Epictetus, Discourses, 2.5.4–5"
-        }
-        const 
-      setQuote(
-        
-      )
+       console.log(error)
     }
   }, [])
 
